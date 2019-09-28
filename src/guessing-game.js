91872c8cc27767545bd59e6aaba658e2a41bf971
Andrey K <karway211@gmail.com>
2019-09-29 01:31:51 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        let center = Math.round((this.right + this.left) / 2);
+        return center;
     }
 
     lower() {
-
+        this.right = Math.round((this.right + this.left) / 2);
     }
 
     greater() {
-
+        this.left = Math.round((this.right + this.left) / 2);
     }
 }
 
